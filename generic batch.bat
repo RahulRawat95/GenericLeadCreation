fe54cd9c37882f98d314f857bbc7b70cc28d35c2
Admin <satyanarayan2003@rediffmail.com>
2018-10-12 12:49:54 +0530

    Initial commit

--- generic batch.bat
+++ generic batch.bat
@@ -1,5 +1,5 @@
 git init
-git remote add origin https://github.com/RahulRawat95/GenericLeadCreation.git
+git remote add origin https://gitlab.com/satyanarayan2003/LeadCreationLibrary.git
 git add .
 git commit -m "Initial commit"
 git push -f origin master
